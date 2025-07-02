3cf5f2b8b64787baba742acbdc47bf6f7a8bed69
jhon-dev <navarro.jhonllyod2005@gmail.com>
2025-07-02 15:04:06 +0800

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='flex-center'>
+      <h1 className='text-xl font-bold'>Pree text</h1>
+    </div>
+  )
+}
+
+export default App
