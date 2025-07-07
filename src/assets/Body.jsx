c9abd7406d03f1df23176501b6384edc9b1df8bd
jhon-dev <navarro.jhonllyod2005@gmail.com>
2025-07-07 20:18:06 +0800

    content were aligned

--- src/assets/Body.jsx
+++ src/assets/Body.jsx
@@ -5,24 +5,51 @@ export default function Body(){
 
   return(
     <div className='motion-preset-fade-lg flex flex-col items-center py-2 '>
-      <section id='Home' className='flex items-center justify-between pt-30 pb-15  flex-col min-h-[85vh] md:min-h-screen '>
-        <div className='flex-center flex-col gap-1 text-white'>
-          <div className='motion-preset-slide-up-md gradient-shadow rounded-full p-2'>
-            <img src={profile} className='w-40 md:w-50 rounded-full border-4 border-[#101010]' alt="" />
-          </div>
-
-          <h1 className='motion-preset-slide-up-md motion-delay-100 font-bold text-4xl md:text-6xl bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent py-3 '>Jhon Llyod Navarro</h1>
-          <h2 className='text-xl md:text-2xl pb-1 motion-preset-slide-up-md motion-delay-200'>Full Stack Developer & UI/UX Designer</h2>
-          <p className=' w-full text-sm px-2 md:w-[60%] md:text-lg motion-preset-slide-up-md motion-delay-300'>I build responsive, functional web applications that address real-world problems. Backed by over a year of experience with modern web technologies and development best practices.</p>
-          <div className='flex-center w-full gap-4 py-4 font-semibold mt-5'>
-            <a href='#Contact' className='btn-anim motion-preset-slide-right-md cursor-pointer bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex-center gap-2 py-2 px-4'><Mail size={18} strokeWidth={1.50}/>Get in Touch</a>
-            <a href='https://github.com/JhonLlyod-dev' target='_blank' className='btn-anim motion-preset-slide-left-md cursor-pointer  border-1 border-pink-600 text-pink-600 hover:bg-pink-600 hover:text-white    rounded-lg flex-center gap-2 py-2 px-4'><Github size={18} strokeWidth={1.50}/>Github</a>
-          </div>
+    <section
+      id="Home"
+      className="flex items-center justify-center pt-30 gap-10 flex-col min-h-[85vh] md:min-h-screen px-6 md:px-10"
+    >
+      <div className="w-full max-w-screen-xl mx-auto flex-center flex-col gap-1 text-white text-center">
+        <div className="motion-preset-slide-up-md gradient-shadow rounded-full p-2">
+          <img
+            src={profile}
+            className="w-40 md:w-50 rounded-full border-4 border-[#101010]"
+            alt=""
+          />
         </div>
 
-        <Mouse className=' motion-translate-y-loop-25 motion-ease-spring-smooth' size={30} strokeWidth={1.50} />
+        <h1 className="motion-preset-slide-up-md motion-delay-100 font-bold text-4xl md:text-6xl bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent py-3">
+          Jhon Llyod Navarro
+        </h1>
+        <h2 className="text-xl md:text-2xl pb-1 motion-preset-slide-up-md motion-delay-200">
+          Full Stack Developer & UI/UX Designer
+        </h2>
+        <p className="w-full md:w-[60%] text-sm md:text-lg motion-preset-slide-up-md motion-delay-300">
+          I build responsive, functional web applications that address real-world problems. Backed by over a year of experience with modern web technologies and development best practices.
+        </p>
 
-      </section>
+        <div className="flex-center w-full gap-4 py-4 font-semibold mt-5">
+          <a
+            href="#Contact"
+            className="btn-anim motion-preset-slide-right-md cursor-pointer bg-gradient-to-r from-blue-600 to-purple-600 rounded-lg flex-center gap-2 py-2 px-4"
+          >
+            <Mail size={18} strokeWidth={1.5} />
+            Get in Touch
+          </a>
+          <a
+            href="https://github.com/JhonLlyod-dev"
+            target="_blank"
+            className="btn-anim motion-preset-slide-left-md cursor-pointer border border-pink-600 text-pink-600 hover:bg-pink-600 hover:text-white rounded-lg flex-center gap-2 py-2 px-4"
+          >
+            <Github size={18} strokeWidth={1.5} />
+            Github
+          </a>
+        </div>
+      </div>
+
+      <Mouse className="motion-translate-y-loop-25 mt-10 motion-ease-spring-smooth" size={30} strokeWidth={1.5} />
+    </section>
+
 
       <section id='About' className='intersect-once intersect:motion-preset-slide-up motion-delay-100 text-gray-400 text-justify flex-center  flex-col md:flex-row gap-10 py-20 px-6 md:px-10 lg:px-34 xl:px-44'>
         <div className='flex-center flex-1/4 flex-col gap-4'>
