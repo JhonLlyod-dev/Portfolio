924789bcd3b04e2b233e6f8c7a4a1271256b7b92
jhon-dev <navarro.jhonllyod2005@gmail.com>
2025-07-07 20:21:50 +0800

    fixed padding

--- src/Pages/Thank.jsx
+++ src/Pages/Thank.jsx
@@ -5,7 +5,7 @@ import {ArrowLeft} from "lucide-react"
 export default function Thanks(){
   return(
     <div className="min-h-screen flex items-center pt-50 flex-col gap-4" >
-      <h1 class="text-7xl font-bold text-gradient-2 p-2">Thank you for reaching out!</h1>
+      <h1 class="text-7xl font-bold text-gradient-2 p-5">Thank you for reaching out!</h1>
       <p class="text-xl text-gray-400 mt-2">Your message has been sent successfully. I’ll get back to you as soon as possible — usually within 24 hours.</p>
       <Link to='/'>
         <button className="mt-10 bg-gradient btn-anim py-2 px-4 font-semibold rounded-lg flex-center gap-4"><ArrowLeft size={20} strokeWidth={2}/>Back to Home</button>
