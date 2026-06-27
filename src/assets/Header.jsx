951f8289a37bbc3cf9d3ee3b50bd86d72128bdf0
jhon-dev <navarro.jhonllyod2005@gmail.com>
2026-06-28 07:53:14 +0800

    Updated Resume

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -26,7 +26,7 @@ export default function Header(){
       </ul>
 
       <div className="flex flex-1 gap-5 justify-end items-center">
-        <a  href="/Resume.pdf" download className=" motion-preset-slide-down-lg motion-delay-600 font-semibold btn-anim text-sm md:text-base flex items-center gap-2 py-2 px-4 rounded-lg bg-gradient-to-r from-blue-600 to-purple-600 "><Download size={16} strokeWidth={2.25} /> Resume</a>
+        <a  href="https://docs.google.com/document/d/1qj_91il-x5rQPbEx0E1lnTbhHYuVW4XiXCmYbF4wLZs/edit?tab=t.0#heading=h.d9dbogz6wawj" target="_blank" className=" motion-preset-slide-down-lg motion-delay-600 font-semibold btn-anim text-sm md:text-base flex items-center gap-2 py-2 px-4 rounded-lg bg-gradient-to-r from-blue-600 to-purple-600 "><Download size={16} strokeWidth={2.25} /> Resume</a>
         <AlignJustify className="md:hidden active:text-blue-600" onClick={navMenu} />
       </div>
 
